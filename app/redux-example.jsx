5ad026ab992101233f453a7b636107177ceafe9c
maniek82 <mariusz.tripler@gmail.com>
2016-10-27 09:55:26 +0100

    add support CHANGE_NAME action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,10 +4,28 @@ console.log('starting redux example');
 
 var reducer = (state= {name: 'Anonymous'}, action)=> {
 	// state = state || {name: 'Anonymous'};//es5
+	console.log('New action ', action);
 
-	return state;
+	switch (action.type) {
+		case 'CHANGE_NAME':
+			return {
+				...state,
+				name: action.name
+			};
+			break;
+		default:
+			return state;
+	}
 };
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('Current State ', currentState);
+
+
+store.dispatch({
+	type: 'CHANGE_NAME',
+	name: 'Mariusz'
+});
+
+console.log('Name should be Mariusz ', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -7,10 +7,28 @@ var stateDefault = {
  todos: []
 };
 var reducer = (state = stateDefault, action) => {
+	switch (action.type) {
+		case 'CHANGE_SEARCH_TEXT':
+			
+			return {
+				...state,
+				searchText: action.searchText
+			};
+			break;
+		default:
+			return state;
+			break;
+	}
 
-return state;
 };
 
 var store = redux.createStore(reducer);
 
-console.log('Current stae ', store.getState());
+console.log('Current state ', store.getState());
+
+
+store.dispatch ({
+	type: 'CHANGE_SEARCH_TEXT',
+	searchText: 'work'
+});
+console.log('search text should be work ', store.getState());
